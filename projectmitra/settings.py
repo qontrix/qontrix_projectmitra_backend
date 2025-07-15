63335e7e870e42ac7376ac0ef93eec344062b505
arconix <134234717+arkodipdas@users.noreply.github.com>
2025-07-15 18:23:03 +0530

    Updated settings.py

--- projectmitra/settings.py
+++ projectmitra/settings.py
@@ -111,12 +111,6 @@ WSGI_APPLICATION = 'projectmitra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
